f5a25b740d8a0a2fe7b60fa301416788589668b0
IGoRFonin <menotoa@gmail.com>
2016-08-25 02:09:25 +0300

    hardcore commit

---
+++ practical_nodejs/4_tests/test-example/test3.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const assert = require('assert');
+let expected, current;
+
+before(() => {
+	expected = ['a', 'b', 'c'];
+});
+
+describe('String#split', () => {
+	beforeEach(() => {
+		current = 'a,b,c'.split(',');
+	});
+	it('should return an array', () => {
+		assert(Array.isArray(current));
+	});
+	it('should return the same array', () => {
+		assert.equal(expected.length, current.length, 'arrays have equal length');
+		for (let i = 0; i < expected.length; i+=1) {
+			assert.equal(expected[i], current[i], i + ' element is equal');
+		}
+	})
+})
