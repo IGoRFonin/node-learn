09d8dbf77cc2ac4827fcac854664fd35ade7bd55
IGoRFonin <menotoa@gmail.com>
2016-08-27 02:05:26 +0300

    consolidate test

---
+++ practical_nodejs/5_templates/express-example.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const express = require('express'),
+	cons = require('consolidate'),
+	app = express();
+
+app.engine('html', cons.swig);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+var platforms = [
+	{ name: 'node' },
+	{ name: 'ruby'},
+	{ name: 'python'}
+]
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		title: 'Consolidate This'
+	})
+})
+
+app.get('/platforms', (req, res) => {
+	res.render('platforms', {
+		title: 'Platforms',
+		platforms: platforms
+	})
+})
+
+app.listen(3000);
+console.log('Express server listening on port 3000');
